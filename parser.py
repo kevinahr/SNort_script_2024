9c76dab443b329d31d9aea27d72f915508b6c670
pythonovice <95148398+pythonovice@users.noreply.github.com>
2024-06-26 19:12:19 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,42 +3,40 @@ This script declare a parse_pcap function that taking in a pcap file and
 parse the following TCP/UDP/ICMP/IP/DNS/payload into a dictionary
 """
 from scapy.all import *
-from scapy.layers.dns import DNS, DNSQR
-from scapy.layers.inet import TCP, UDP, IP, ICMP
-from scapy.layers.l2 import Ether
 
-pcap_file = 'your_pcap_file.pcap'  # Replace with your actual pcap file
+packets = '/Users/hieu/PycharmProjects/pythonProject/test.pcapng'  # Replace with your actual pcap file location
 
-def parse_pcap(pcap_file):
-    packets = rdpcap(pcap_file)
-    parse_data = {}
-    for packet in packets:
-        if IP in packet:
-            parse_data['ip_src'] = packet[IP].src
-            parse_data['ip_dst'] = packet[IP].dst
-        if Ether in packet:
-            parse_data['src_mac'] = packet[Ether].src
-            parse_data['dst_mac'] = packet[Ether].dst
-        if TCP in packet:
+
+def parse(x):
+    file = PcapReader(packets)
+    for packet in file:
+        parse_data = {}
+        parse_data['Timestamp'] = packet.time
+        if 'IP' in packet:
+            parse_data['ip_src'] = packet['IP'].src
+            parse_data['ip_dst'] = packet['IP'].dst
+        if 'Ether' in packet:
+            parse_data['src_mac'] = packet['Ether'].src
+            parse_data['dst_mac'] = packet['Ether'].dst
+        if 'TCP' in packet:
             parse_data['protocol'] = 'TCP'
-            parse_data['tcp_scr'] = packet[TCP].sport
-            parse_data['tcp_dst'] = packet[TCP].dport
-            if packet[TCP].flags & 0x02:
+            parse_data['tcp_scr'] = packet['TCP'].sport
+            parse_data['tcp_dst'] = packet['TCP'].dport
+            if packet['TCP'].flags & 0x02:
                 parse_data['suspicious_tcp_syn'] = packet.summary()
-        if UDP in packet:
+        if 'UDP' in packet:
             parse_data['protocol'] = 'UDP'
-            parse_data['udp_src'] = packet[UDP].sport
-            parse_data['udp_dst'] = packet[UDP].dport
-        if ICMP in packet:
+            parse_data['udp_src'] = packet['UDP'].sport
+            parse_data['udp_dst'] = packet['UDP'].dport
+        if 'ICMP' in packet:
             parse_data['protocol'] = 'ICMP'
-            parse_data['icmp_type'] = packet[ICMP].type
-            parse_data['icmp_code'] = packet[ICMP].code
-        if DNS in packet:
-            parse_data['dns_query'] = packet[DNSQR].qname.decode('utf-8')
-            parse_data['dns_response'] = packet[DNSQR].rdata
-            parse_data['dns_query_type'] = packet[DNSQR].qtype
-            if len(packet[DNS].payload) > 100:
-                parse_data['Suspicious_dns_tunneling'] = packet.summary()
-        if Raw in packet:
-            parse_data['payload'] = packet[Raw].load
-    return parse_data
+            parse_data['icmp_type'] = packet['ICMP'].type
+            parse_data['icmp_code'] = packet['ICMP'].code
+        if 'Raw' in packet:
+            parse_data['payload'] = packet['Raw'].load
+        if 'DNS' in packet:
+            parse_data['dns_query_type'] = packet['DNS'].qd
+            parse_data['dns_response'] = packet['DNS'].ns
+
+        print(parse_data)
+        
